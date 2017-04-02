14dc9ebb46aaad41cd3f25c21f391a7bbc263fec
Kostiantyn Rybnikov <k-bx@k-bx.com>
2017-04-02 21:00:17 +0300

    Initial non-working version

---
+++ src/Main.hs
@@ -0,0 +1,12 @@
+module Main where
+
+import Data.Array.Accelerate as A
+import Data.Array.Accelerate.LLVM.Native as CPU
+-- import Data.Array.Accelerate.LLVM.PTX     as GPU
+
+dotp :: Acc (Vector Float) -> Acc (Vector Float) -> Acc (Scalar Float)
+dotp xs ys = A.fold (+) 0 (A.zipWith (*) xs ys)
+
+main :: IO ()
+main = do
+  putStrLn "hello world"

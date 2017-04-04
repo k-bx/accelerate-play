f5324edbc7304d55bbf30e30819a8f53431de6a0
Kostiantyn Rybnikov <k-bx@k-bx.com>
2017-04-04 10:18:57 +0300

    Some instructions from GitHub

--- src/Main.hs
+++ src/Main.hs
@@ -1,12 +1,15 @@
+{-# LANGUAGE OverloadedLists #-}
+
 module Main where
 
 import Data.Array.Accelerate as A
 import Data.Array.Accelerate.LLVM.Native as CPU
--- import Data.Array.Accelerate.LLVM.PTX     as GPU
+import Data.Array.Accelerate.LLVM.PTX as GPU
 
 dotp :: Acc (Vector Float) -> Acc (Vector Float) -> Acc (Scalar Float)
 dotp xs ys = A.fold (+) 0 (A.zipWith (*) xs ys)
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  putStrLn "hey"
+  print (GPU.run (dotp (use []) (use [])))
